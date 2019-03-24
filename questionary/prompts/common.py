eb7eb5f10432f72622403daf718a380cb4a9a9e2
Ahmed Agiza <ahmed.agia22@gmail.com>
2019-03-24 21:18:04 +0200

    Fix Default Pointer for Choice Classes

--- questionary/prompts/common.py
+++ questionary/prompts/common.py
@@ -143,14 +143,17 @@ class InquirerControl(FormattedTextControl):
         # helper to convert from question format to internal format
         self.choices = []
 
+        strChoices = [choice if choice is isinstance(
+            choice, str) else choice.value for choice in choices]
+
         if self.pointed_at is None and self.default is not None:
             try:
                 if type(self.default) == list:
-                    index = choices.index(self.default[0])
+                    index = strChoices.index(self.default[0])
                     if index >= 0:
                         self.pointed_at = index
                 else:
-                    index = choices.index(self.default)
+                    index = strChoices.index(self.default)
                     if index >= 0:
                         self.pointed_at = index
             except ValueError:
